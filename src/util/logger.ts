1b8c4ed899096df224430c9362f30a16e2346f40
Squ1ggly <60225348+Squ1ggly@users.noreply.github.com>
2026-03-21 23:52:35 +1000

    Clean up logger.ts by removing unused code
    Removed commented-out code for elasticsearch logging.

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -17,17 +17,6 @@ if (process.env.NODE_ENV !== "production") {
     }
   });
 }
-// Leaving this here for future use if you want to use elasticsearch for logging.
-// targets.push({
-//   target: "pino-elasticsearch",
-//   level: LOG_LEVEL,
-//   options: {
-//     node: {elasticSearchNodeUrl},
-//     index: {elasticSearchIndex},
-//     esVersion: 8,
-//     flushBytes: 1000
-//   }
-// });
 
 const logger = pino({
   level: LOG_LEVEL,
